753e6ee3af68428b7f21601a337eb2207f974d73
foohaya <foohaya@gmail.com>
2025-04-26 21:32:01 +0900

    バリテーション記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,32 @@
 class Item < ApplicationRecord
+  belongs_to :user
+  has_one :order
+  has_one_attached :image
+
+  with_options presence: true do
+    validates :image
+    validates :name
+    validates :info
+    validates :category_id
+    validates :sales_status_id
+    validates :shipping_fee_id
+    validates :prefecture_id
+    validates :scheduled_delivery_id
+    validates :price
+  end
+
+  with_options numericality: { other_than: 1, message: "can't be blank" } do
+    validates :category_id
+    validates :sales_status_id
+    validates :shipping_fee_id
+    validates :prefecture_id
+    validates :scheduled_delivery_id
+  end
+
+  validates :price, numericality: { 
+    only_integer: true, 
+    greater_than_or_equal_to: 300, 
+    less_than_or_equal_to: 9_999_999,
+    message: 'is out of setting range'
+  }
 end
