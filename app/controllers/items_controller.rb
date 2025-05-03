5931bafc2a0766f15e145d541c3c1680e8366e53
foohaya <foohaya@gmail.com>
2025-05-03 23:56:58 +0900

    修正２

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
 
   def index
-    @items = []
+    #@items = Item.all
   end
 
   def show
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -89,5 +89,11 @@ RSpec.describe Item, type: :model do
       @item.valid?
       expect(@item.errors.full_messages).to include('Price is out of setting range')
     end
+
+    it 'ユーザーが紐づいていないと出品できない' do
+      @item.user = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("User must exist")
+    end
   end
 end
